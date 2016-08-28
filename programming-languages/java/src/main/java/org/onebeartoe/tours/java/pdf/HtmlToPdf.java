62b7c2deb0a77a1f29aeac8ad941587892847d66
onebeartoe <onebeartoe@gmail.com>
2016-08-28 13:11:18 -0500

    The test for HTML to PDF conversion now includes a live URL reading.  No images are in the conversion, yet.

--- programming-languages/java/src/main/java/org/onebeartoe/tours/java/pdf/HtmlToPdf.java
+++ programming-languages/java/src/main/java/org/onebeartoe/tours/java/pdf/HtmlToPdf.java
@@ -10,10 +10,13 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import com.itextpdf.tool.xml.XMLWorkerHelper;
+import java.io.BufferedReader;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 /**
  *
@@ -28,15 +31,55 @@ public class HtmlToPdf
 {
     public static void main(String [] args) throws FileNotFoundException, DocumentException, IOException
     {
-        String k = "<html><body> This is my Project </body></html>";
-        File outfile = new File("./test.pdf");
+        HtmlToPdf converter = new HtmlToPdf();
+        
+        String rawHtml = "<html><body> This is my <br/> Project </body></html>";
+        String outfile = "./test.pdf";
+        converter.htmlToPdf(rawHtml, outfile);
+
+        String url = "http://electronics.onebeartoe.org/3d-realization/printers/printrbot-simple-metal/upgrades/lcd-with-rotary-button/assembly";
+               url = "http://localhost:8080/electronics/3d-printing/printers/printrbot-simple-metal/upgrades/lcd-with-rotary-button/assembly/";
+                
+        URL inputUrl = new URL(url);        
+        String urlOutfile = "./url-test.pdf";
+        converter.htmlToPdf(inputUrl, urlOutfile);
+    }
+    
+    public void htmlToPdf(String html, String outfilePath) throws FileNotFoundException, DocumentException, IOException
+    {
+        File outfile = new File(outfilePath);
         OutputStream file = new FileOutputStream(outfile);
+        
         Document document = new Document();
         PdfWriter writer = PdfWriter.getInstance(document, file);
         document.open();
-        InputStream is = new ByteArrayInputStream(k.getBytes());
+        
+        InputStream is = new ByteArrayInputStream(html.getBytes());
         XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
         document.close();
-        file.close();
+        file.close();        
+    }
+    
+    public void htmlToPdf(URL inputUrl, String outfilePath) throws FileNotFoundException, DocumentException, IOException
+    {
+        String html = readUrlContents(inputUrl);
+        
+        htmlToPdf(html, outfilePath);
+    }
+    
+    private String readUrlContents(URL inputUrl) throws IOException
+    {
+        StringBuilder sb = new StringBuilder();
+        
+        InputStream is = inputUrl.openStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        br.lines().forEach(line ->
+        {
+            sb.append(line);
+            sb.append(System.lineSeparator());
+        });
+        
+        return sb.toString();
     }
 }
