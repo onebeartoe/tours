6972f4f0e289a709238f667ca10869fea7f9f6a2
Roberto Marquez <onebeartoe@gmail.com>
2019-08-31 14:17:57 -0500

    show an emoji on the UI

--- programming-languages/emojis/src/main/java/org/onebeartoe/tours/emojis/FXMLController.java
+++ programming-languages/emojis/src/main/java/org/onebeartoe/tours/emojis/FXMLController.java
@@ -6,20 +6,34 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController implements Initializable 
+{
     @FXML
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private GridPane gridPane;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) 
+    {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
     
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+//        String bear = "🐻";
+        
+        // If the previous line doesn't show up in your editor,
+        // you can comment it out and use this declaration instead: 
+        String bear = "\ud83d\udc3b";
+        
+        Label bearLabel = new Label(bear);
+        
+        gridPane.add(bearLabel, 0, 0);
     }    
 }
