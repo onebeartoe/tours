8f0d42fc05e6ca49f50218d9bc388e61966ca9a3
Roberto Marquez <onebeartoe@gmail.com>
2020-01-05 22:50:17 -0600

    add some more PDF TODOs

--- programming-languages/java/src/main/java/org/onebeartoe/tours/pdf/HtmlToPdf.java
+++ programming-languages/java/src/main/java/org/onebeartoe/tours/pdf/HtmlToPdf.java
@@ -3,13 +3,20 @@ package org.onebeartoe.tours.pdf;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfWriter;
+
+import com.itextpdf.tool.xml.html.HTMLUtils;
+
 import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import com.itextpdf.tool.xml.XMLWorkerHelper;
+import com.itextpdf.tool.xml.html.Div;
+import com.itextpdf.tool.xml.html.HTML.Tag;
+import com.itextpdf.tool.xml.html.Span;
 import java.io.BufferedReader;
 
 import java.io.File;
@@ -19,16 +26,28 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 /**
- *
- * @author Roberto Marquez
- * 
- *//**
- *
- * @author Roberto Marquez
- * 
+ * This class demonstrates PDF generation with the iText APIs.
  */
 public class HtmlToPdf 
 {
+    public void testTags()
+    {
+        Div div = new Div();
+        
+        Span s = new Span();
+        
+//        Tag t = new Tag();
+        
+//        t.toString();
+
+        HTMLUtils utils = (new HTMLUtils());
+//        utils.
+        
+//        HTML html = HTML();
+        
+//just use org.onebeartoe.html.CodeGenerator#htmlify()        
+    }
+    
     public static void main(String [] args) throws FileNotFoundException, DocumentException, IOException
     {
         HtmlToPdf converter = new HtmlToPdf();
@@ -37,12 +56,21 @@ public class HtmlToPdf
         String outfile = "./test.pdf";
         converter.htmlToPdf(rawHtml, outfile);
 
+//TODO: change this to the URL of the readme.md file for this project.        
         String url = "http://electronics.onebeartoe.org/3d-realization/printers/printrbot-simple-metal/upgrades/lcd-with-rotary-button/assembly";
-               url = "http://localhost:8080/electronics/3d-printing/printers/printrbot-simple-metal/upgrades/lcd-with-rotary-button/assembly/";
-                
+//               url = "http://localhost:8080/electronics/3d-printing/printers/printrbot-simple-metal/upgrades/lcd-with-rotary-button/assembly/";
+                url = "http://electronics.onebeartoe.org/";
+
         URL inputUrl = new URL(url);        
         String urlOutfile = "./url-test.pdf";
         converter.htmlToPdf(inputUrl, urlOutfile);
+        
+        
+//TODO: writes some tests to verify the HTML content is actually in the PDF file.        
+        InputStream instream = null;
+        PdfReader reader = new PdfReader(instream);
+//TODO: read all contents of the PDF file.        
+//        reader.get
     }
     
     public void htmlToPdf(String html, String outfilePath) throws FileNotFoundException, DocumentException, IOException
@@ -57,7 +85,7 @@ public class HtmlToPdf
         InputStream is = new ByteArrayInputStream(html.getBytes());
         XMLWorkerHelper.getInstance().parseXHtml(writer, document, is);
         document.close();
-        file.close();        
+        file.close();
     }
     
     public void htmlToPdf(URL inputUrl, String outfilePath) throws FileNotFoundException, DocumentException, IOException
