bc8d659aa9a49cd02da5e3cc01e2b8cf22d585f9
Roberto Marquez <onebeartoe@users.noreply.github.com>
2023-02-06 11:05:41 -0600

    Create VfwApplication.java
    Add an introvert program for Whitney.

---
+++ programming-languages/java/src/main/java/org/onebeartoe/tours/VfwApplication.java
@@ -0,0 +1,20 @@
+
+package org.onebeartoe.tours;
+
+/**
+ * This application demonstrates numbers.
+ * @author Roverto
+ */
+public class VfwApplication
+{
+    public static void main(String[] args) 
+    {
+        int rovert = 44;        
+        
+        int whitney = 46;
+
+        System.out.println("whitney = " + whitney);
+        
+        System.out.println("rovert = " + rovert);
+    }
+}
