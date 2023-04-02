5710db29cb3918b8861a2cddc38a881d858e818e
Roberto Marquez <onebeartoe@gmail.com>
2023-04-02 05:45:22 -0500

    add a starting point for Spring Batch fixed length flatfile reader

---
+++ programming-languages/java/spring/batch/console/src/main/java/org/onebeartoe/tours/spring/console/BatchConfiguration.java
@@ -0,0 +1,124 @@
+
+package org.onebeartoe.tours.spring.console;
+
+import com.example.batchprocessing.JobCompletionNotificationListener;
+import com.example.batchprocessing.Person;
+import com.example.batchprocessing.PersonItemProcessor;
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.LineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class BatchConfiguration {
+
+	// tag::readerwriterprocessor[]
+	@Bean
+	public FlatFileItemReader<Person> reader() 
+        {
+// the contents of this method are from:
+//         github/Manfred73/spring-batch-demo/src/main/java/nl/craftsmen/contact/job/reader/ProcessContactsFileReaderConfig.java
+String filename = "sample-data.csv";
+
+LineTokenizer lineTokenizer = new ContactsLineTokenizer().createLineTokenizer();
+
+final var lineMapper = createLineMapper(lineTokenizer);
+
+final var logger = new ConditionalLogger();
+final var skipRecordCallback = new SkipRecordCallback(logger);
+
+            FlatFileItemReader<ContactWrapper> reader = new FlatFileItemReader<>();
+            reader.setResource(new ClassPathResource(filename));
+            reader.setLinesToSkip(1);
+            reader.setSkippedLinesCallback(skipRecordCallback);
+            reader.setLineMapper(lineMapper);
+            
+//            Person 
+            
+            return reader;
+	}    
+
+	@Bean
+	public FlatFileItemReader<Person> reader_old() {
+		return new FlatFileItemReaderBuilder<Person>()
+			.name("personItemReader")
+			.resource(new ClassPathResource("sample-data.csv"))
+			.delimited()
+			.names(new String[]{"firstName", "lastName"})
+			.fieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
+				setTargetType(Person.class);
+			}})
+			.build();
+	}        
+        
+        
+    private LineMapper<ContactWrapper> createLineMapper(LineTokenizer lineTokenizer) 
+    {
+        final var contactsFileRowMapper = new ContactsFileRowMapper();
+        
+        final var mapper = new DefaultLineMapper<ContactWrapper>();
+        mapper.setLineTokenizer(lineTokenizer);
+        mapper.setFieldSetMapper(contactsFileRowMapper);
+        
+        return mapper;
+    }        
+        
+        
+
+
+	@Bean
+	public PersonItemProcessor processor() {
+		return new PersonItemProcessor();
+	}
+
+	@Bean
+	public JdbcBatchItemWriter<Person> writer(DataSource dataSource) {
+		return new JdbcBatchItemWriterBuilder<Person>()
+			.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+			.sql("INSERT INTO people (first_name, last_name) VALUES (:firstName, :lastName)")
+			.dataSource(dataSource)
+			.build();
+	}
+	// end::readerwriterprocessor[]
+
+	// tag::jobstep[]
+	@Bean
+	public Job importUserJob(JobRepository jobRepository,
+			JobCompletionNotificationListener listener, Step step1) {
+		return new JobBuilder("importUserJob", jobRepository)
+			.incrementer(new RunIdIncrementer())
+			.listener(listener)
+			.flow(step1)
+			.end()
+			.build();
+	}
+
+	@Bean
+	public Step step1(JobRepository jobRepository,
+			PlatformTransactionManager transactionManager, JdbcBatchItemWriter<Person> writer) {
+		return new StepBuilder("step1", jobRepository)
+			.<Person, Person> chunk(10, transactionManager)
+			.reader(reader())
+			.processor(processor())
+			.writer(writer)
+			.build();
+	}
+	// end::jobstep[]
+}
--- programming-languages/java/spring/batch/console/src/main/java/org/onebeartoe/tours/spring/console/Console.java
+++ programming-languages/java/spring/batch/console/src/main/java/org/onebeartoe/tours/spring/console/Console.java
@@ -1,16 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package org.onebeartoe.tours.spring.console;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- *
- * @author roberto
+ * This is a sample Spring Batch application using a fixed length flatfile reader 
+ * and a database writer.
+ * 
+ * The starting point for this application was this example
+ * 
+ *          https://spring.io/guides/gs/batch-processing/
  */
+@SpringBootApplication
 public class Console {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println("Hello World!");
+        
+        System.exit(
+                SpringApplication.exit(
+                        SpringApplication.run(
+                                Console.class, args)));
     }
 }
