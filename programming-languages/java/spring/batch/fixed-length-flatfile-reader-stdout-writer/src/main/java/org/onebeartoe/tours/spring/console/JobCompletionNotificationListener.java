04f647c354b868010eac9c2a9825bf86194c904e
Roberto Marquez <onebeartoe@gmail.com>
2023-04-03 07:57:57 -0500

    remove JDBC call

--- programming-languages/java/spring/batch/fixed-length-flatfile-reader-stdout-writer/src/main/java/org/onebeartoe/tours/spring/console/JobCompletionNotificationListener.java
+++ programming-languages/java/spring/batch/fixed-length-flatfile-reader-stdout-writer/src/main/java/org/onebeartoe/tours/spring/console/JobCompletionNotificationListener.java
@@ -1,3 +1,4 @@
+
 package org.onebeartoe.tours.spring.console;
 
 import com.example.batchprocessing.Person;
@@ -11,9 +12,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JobCompletionNotificationListener implements JobExecutionListener {
-
-	private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+public class JobCompletionNotificationListener implements JobExecutionListener
+{
+    private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
 	private final JdbcTemplate jdbcTemplate;
 
@@ -22,16 +23,15 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 
-	@Override
-	public void afterJob(JobExecution jobExecution) {
-		if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
-			log.info("!!! JOB FINISHED! Time to verify the results");
-
-			jdbcTemplate.query("SELECT first_name, last_name FROM people",
-				(rs, row) -> new Person(
-					rs.getString(1),
-					rs.getString(2))
-			).forEach(person -> log.info("Found <{{}}> in the database.", person));
-		}
-	}
+    @Override
+    public void afterJob(JobExecution jobExecution)
+    {
+        if(jobExecution.getStatus() == BatchStatus.COMPLETED)
+        {
+            log.info("!!! JOB FINISHED! Time to verify the results");
+            
+//            jobExecution.getEndTime()
+//            jobExecution.getStartTime()
+        }
+    }
 }
