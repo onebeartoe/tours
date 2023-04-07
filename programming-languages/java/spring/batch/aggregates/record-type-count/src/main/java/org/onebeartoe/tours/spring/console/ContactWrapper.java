9511a7baf10bebb63e3db37bb5be24ef3688fb54
Roberto Marquez <onebeartoe@gmail.com>
2023-04-07 09:49:58 -0500

    whitespace

---
+++ programming-languages/java/spring/batch/aggregates/record-type-count/src/main/java/org/onebeartoe/tours/spring/console/ContactWrapper.java
@@ -0,0 +1,32 @@
+
+package org.onebeartoe.tours.spring.console;
+
+//import nl.craftsmen.contact.model.*;
+import com.example.batchprocessing.Person;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@JsonPropertyOrder({
+		"contactRecord",
+		"contact"
+})
+public class ContactWrapper {
+
+	@NotNull
+	private String contactRecord;
+
+	@NotNull
+	private Person contact;
+}
---
+++ programming-languages/java/spring/batch/aggregates/record-type-count/src/main/java/org/onebeartoe/tours/spring/console/PigFileRowMapper.java
@@ -0,0 +1,28 @@
+
+package org.onebeartoe.tours.spring.console;
+
+import com.example.batchprocessing.Person;
+import lombok.NonNull;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+
+/**
+ *
+ */
+public class PigFileRowMapper implements FieldSetMapper<Person>
+{    
+    @Override
+    public @NonNull Person mapFieldSet(FieldSet fieldSet) 
+    {
+        var first = fieldSet.readString(PigFieldnames.ENTITY);
+        var last = fieldSet.readString(PigFieldnames.OWNER);
+        
+var distance = fieldSet.readString(PigFieldnames.DISTANCE);
+
+        Person p = new Person(first, last);
+//TODO: set the distance on the Pig type. 
+//      p.distance = distance;        
+        
+        return p;
+    }
+}
