f21f21b57fbd6b9f567c094e6f7fe00c1aa82d0c
Roberto Marquez <onebeartoe@gmail.com>
2024-03-01 06:01:05 -0600

    add a Lambda example

---
+++ programming-languages/java/src/test/java/org/onebeartoe/tours/lambdas/LambdasTest.java
@@ -0,0 +1,35 @@
+
+package org.onebeartoe.tours.lambdas;
+
+//import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertNotEquals;
+import org.testng.annotations.Test;
+
+
+//import org.testng.annotations.Test;
+
+
+public class LambdasTest 
+{
+    @Test
+    public void objectStateMutaionOccurs()
+    {
+        var originalValue = 33;
+        
+        int[] total = new int[1];
+
+        total[0] = originalValue;
+
+        System.out.println("total[0] =  " + total[0]);
+
+        Runnable r = () -> total[0]++;
+
+        r.run();
+
+        System.out.println("after total[0] =  " + total[0]);
+
+        assertNotEquals(total[0], originalValue);
+    }
+    
+}
